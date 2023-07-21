7599dac6cc95fb6519bdf5fbaf417f55c1446c8a
kenendy2daniel <daniel.kennedy@co.washington.pa.us>
2023-07-21 11:08:05 -0400

    init

--- src/App.js
+++ src/App.js
@@ -1,23 +1,106 @@
+import ReactDOM from 'react-dom';
 import React from 'react';
-import logo from './logo.svg';
+//import logo from 'new-county-logo.png';
 import './App.css';
+import Navbar from './components/Navbar';
+// Font Awesome -- Start
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faBus } from "@fortawesome/free-solid-svg-icons"; // Transportation needs
+import { faWheatAwnCircleExclamation as faFood } from '@fortawesome/free-solid-svg-icons'; // Food insecurity      
+import { faHouse } from '@fortawesome/free-solid-svg-icons';      // Housing insecurity
+import { faChild } from '@fortawesome/free-solid-svg-icons';
+import { faUserDoctor } from '@fortawesome/free-solid-svg-icons';
+import { faRibbon } from '@fortawesome/free-solid-svg-icons';
+import { faBriefcase } from '@fortawesome/free-solid-svg-icons';
+import { faPeopleGroup } from '@fortawesome/free-solid-svg-icons';
+import { faPhone } from '@fortawesome/free-solid-svg-icons';
+// Font Awesome -- End
 
 function App() {
   return (
     <div className="App">
+      <Navbar />
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        {/* First row of icons */}
+        <div class="container mt-4">
+          <h1 className="display-3" style={{ color: "bisque" }}>
+            The Help Center
+          </h1>
+          <hr style={{ color: "bisque" }} />
+          <div id="iconContainer" className="row text-center">
+            <div className="col-sm-3">
+              <a href="https://washingtonpa.findhelp.com/search/text?term=transportation&postal=15301&language=en">
+                <FontAwesomeIcon icon={faBus} style={{ height: "5rem" }} />
+                <p style={{ color: "bisque", fontSize: "1.2rem" }}>Transportation</p>
+              </a>
+            </div>
+            <div className="col-sm-3">
+              <a href="https://washingtonpa.findhelp.com/search/text?term=food&postal=15301&language=en">
+                <FontAwesomeIcon icon={faFood} style={{ height: "5rem" }} />
+                <p style={{ color: "bisque", fontSize: "1.2rem" }}>Food</p>
+              </a>
+            </div>
+            <div className="col-sm-3">
+              <a href="https://washingtonpa.findhelp.com/search/text?term=housing&postal=15301&language=en">
+                <FontAwesomeIcon icon={faHouse} style={{ height: "5rem" }} />
+                <p style={{ color: "bisque", fontSize: "1.2rem" }}>Housing</p>
+              </a>
+            </div>
+          </div>
+          {/* Second row of icons */}
+          <div id="iconContainer" className="row text-center">
+
+            <div className="col-sm-3">
+              <a href="https://washingtonpa.findhelp.com/search/text?term=childcare&postal=15301&language=en">
+                <FontAwesomeIcon icon={faChild} style={{ height: "5rem" }} />
+
+                <p style={{ color: "bisque", fontSize: "1.2rem" }}>Childcare</p>
+              </a>
+            </div>
+            <div className="col-sm-3">
+              <a href="https://washingtonpa.findhelp.com/search/text?term=healthcare&postal=15301&language=en">
+                <FontAwesomeIcon icon={faUserDoctor} style={{ height: "5rem" }} />
+                <p style={{ color: "bisque", fontSize: "1.2rem" }}>All Health</p>
+              </a>
+            </div>
+            <div className="col-sm-3">
+              <a href="https://washingtonpa.findhelp.com/search/text?term=addiction&postal=15301&language=en">
+                <FontAwesomeIcon icon={faRibbon} style={{ height: "5rem" }} className="text-info" />
+                <p style={{ color: "bisque", fontSize: "1.2rem" }}>Addiction</p>
+              </a>
+            </div>
+          </div>
+          {/* Third row of icons */}
+          <div id="iconContainer" className="row text-center">
+            <div className="col-sm-3">
+              <a href="https://washingtonpa.findhelp.com/search/text?term=employment&postal=15301&language=en">
+                <FontAwesomeIcon icon={faBriefcase} style={{ height: "5rem" }} />
+                <p style={{ color: "bisque", fontSize: "1.2rem" }}>Employment</p>
+              </a>
+            </div>
+            <div className="col-sm-3">
+              <a href="https://washingtonpa.findhelp.com/search/text?term=domestic%20violence&postal=15301&language=en">
+                <FontAwesomeIcon icon={faRibbon} style={{ height: "5rem", color: "purple", opacity: "0.6" }} />
+                <p style={{ color: "bisque", fontSize: "1.2rem" }}>Domestic Violence</p>
+              </a>
+
+            </div>
+            <div className="col-sm-3">
+              <a href="https://washingtonpa.findhelp.com/search/text?term=social%20connections&postal=15301&language=en">
+                <FontAwesomeIcon icon={faPeopleGroup} style={{ height: "5rem" }} />
+                <p style={{ color: "bisque", fontSize: "1.2rem" }}>Social Connections</p>
+              </a>
+            </div>
+          </div>
+          <h4 className="display-4 mt-4" style={{ color: "bisque" }}>
+            <a href="tel:7242281234" style={{ textDecoration: "none", color: "bisque" }}>
+              <FontAwesomeIcon icon={faPhone} style={{ color: "var(--bs-danger)" }} /> Call 724-228-1234
+            </a>
+          </h4>
+          <div id="googleMap" className="mt-4">
+            <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d906.662697594959!2d-80.24795298328961!3d40.170709504345645!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x8835adbb6b19e47d%3A0x26d05e2663c41f54!2s95%20W%20Beau%20St%2C%20Washington%2C%20PA%2015301!5e0!3m2!1sen!2sus!4v1689948452442!5m2!1sen!2sus" width="100%" height="450" style={{ border: 0 }} allowFullscreen="" loading="lazy" referrerPolicy="no-referrer-when-downgrade"></iframe>
+          </div>
+        </div>
       </header>
     </div>
   );
---
+++ src/components/Navbar.js
@@ -0,0 +1,104 @@
+import React, { useState } from 'react';
+import { Button, Nav, Navbar, ListGroup, ListGroupItem } from 'react-bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Modal from 'react-bootstrap/Modal';
+//import Form from 'react-bootstrap/Form';
+import { TelephoneOutbound } from 'react-bootstrap-icons';
+
+function MainNavbar() {
+    // for hotline modal
+    const [show, setShow] = useState(false);
+    const handleClose = () => setShow(false);
+    const handleShow = () => setShow(true);
+    return (
+        <>
+            <Navbar  style={{ backgroundColor: '#ece9de' }} className='shadow-lg'>
+                <Navbar.Brand href='#home' style={{margin:'0 2rem'}}>
+                    <img
+                        src='new-county-logo.png'
+                        width='60'
+                        height='60'
+                        className='d-inline-block align-top'
+                        alt='React Bootstrap logo'
+                    />
+                    <Navbar.Text id="navbarTitle" style={{verticalAlign:"middle", fontSize:'1.5rem', fontWeight:'bold', color:'var(--bs-dark)'}}>Washington County, PA</Navbar.Text>
+                </Navbar.Brand>
+                <Navbar.Toggle aria-controls='basic-navbar-nav' />
+                <Navbar.Collapse id='basic-navbar-nav' style={{margin:'0 2rem'}}>
+                    <Nav className='ms-auto'>
+                        <a href='#home' className='btn btn-danger' onClick={handleShow}>
+                            <TelephoneOutbound />&nbsp; Hotlines
+                        </a>
+                    </Nav>
+                </Navbar.Collapse>
+            </Navbar>
+            <Modal show={show} onHide={handleClose}>
+                <Modal.Header closeButton style={{backgroundColor:"#ece9de"}}>
+                    <Modal.Title style={{textAlign:'left'}}>Washington County<br />
+                    Department of Human Services
+                    </Modal.Title>
+                </Modal.Header>
+                <Modal.Body style={{backgroundColor:"#007575"}}>
+                <p className="bg-danger text-light p-2 text-center">
+                    <TelephoneOutbound />
+                    <h4>HELP CENTER<br /><a href="tel:7242281234" className="text-light">724-228-1234</a></h4>
+                </p>
+                    <ListGroup id="hotlineList">
+                        <ListGroupItem>
+                            <a href="tel:1-800-932-0313">
+                            Child Abuse Hotline<br /><strong>1-800-932-0313</strong>
+                            </a>
+                        </ListGroupItem>
+                        <ListGroupItem>
+                        <a href="tel:724-223-9190">
+                            Domestic Violence Hotline<br /><strong>724-223-9190</strong>
+                        </a>
+                        </ListGroupItem>
+                        <ListGroupItem>
+                            <a href="tel:1-800-434-4357">
+                            Drug and Alcohol Crisis<br /><strong>1-800-GET-HELP</strong>
+                            </a>
+                        </ListGroupItem>
+                        <ListGroupItem>
+                            <a href="tel:1-800-373-7888">
+                            Human Trafficking<br /><strong>1-888-373-7888</strong>
+                            </a>
+                            </ListGroupItem>
+                        <ListGroupItem>
+                            <a href="tel:1-877-225-3567">
+                            Mental Health Crisis<br /><strong>1-877-225-3567</strong>
+                            </a>
+                        </ListGroupItem>
+                        <ListGroupItem>
+                            <a href="tel:1-800-537-2424">
+                            Older Adult Proective Services<br /><strong>1-800-537-2424</strong>
+                            </a>
+                        </ListGroupItem>
+                        <ListGroupItem>
+                            <a href="tel:1-800-480-7283">
+                            SSTARS Sexual Assault<br /><strong>1-888-480-7283</strong>
+                            </a>
+                        </ListGroupItem>
+                        <ListGroupItem>
+                            <a href="tel:1-800-273-8255">
+                            Suicide Prevention<br /><strong>988 or 1-800-273-8255</strong>
+                            </a>
+                        </ListGroupItem>
+                        <ListGroupItem>
+                            <a href="tel:1-800-273-8255">
+                            Veterans Crisis Line<br /><strong>1-800-273-8255 Press 1</strong>
+                            </a>
+                        </ListGroupItem>
+                    </ListGroup>
+                </Modal.Body>
+                <Modal.Footer style={{backgroundColor:"#ece9de"}}>
+                    <Button variant='danger' onClick={handleClose}>
+                        Close
+                    </Button>
+                </Modal.Footer>
+            </Modal>
+        </>
+    )
+}
+
+export default MainNavbar;
